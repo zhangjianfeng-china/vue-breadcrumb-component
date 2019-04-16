1af9953ad32f4aff103db8bd9d16d9a9b679711a
Essjs <35795021+Essjs@users.noreply.github.com>
2019-04-16 14:14:41 +0800

    Add files via upload

---
+++ breadcrumb/src/breadcrumb-item.vue
@@ -0,0 +1,53 @@
+<template>
+  <span class="so-breadcrumb_item" ref="link">
+    <a :href="to" class="label-name">
+      <slot></slot>
+    </a>
+    <i v-if="separatorClass" class="separator_icon" :class="separatorClass"></i>
+    <span v-else class="separator">{{separator}}</span>
+  </span>
+</template>
+<style scoped>
+.so-breadcrumb_item {
+  font-size: 14px;
+  color: #303133;
+}
+.separator {
+  color: #c0c4cc;
+  margin: 0 9px;
+  font-weight: 700;
+}
+.label-name {
+  color: #303133;
+  font-weight: 700;
+}
+.separator_icon {
+  margin: 0 9px;
+  color: #c0c4cc;
+}
+</style>
+<script>
+export default {
+  name: "",
+  //   props: {
+  //     to: {},
+  //     replace: Boolean
+  //   },
+  props: ["to"],
+  data() {
+    return {
+      separator: "",
+      separatorClass: ""
+    };
+  },
+  inject: ["Sobreadcrumb"],
+  /** 载入后 */
+  mounted() {
+    this.separator = this.Sobreadcrumb.separator;
+    this.separatorClass = this.Sobreadcrumb.separatorClass;
+  },
+  methods: {}
+};
+</script>
+
+
---
+++ breadcrumb/src/breadcrumb.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="so-breadcrumb">
+    <slot></slot>
+  </div>
+</template>
+<style>
+.so-breadcrumb_item:last-child .separator {
+  display: none;
+}
+.so-breadcrumb_item:last-child .label-name {
+  color: #606266;
+}
+.so-breadcrumb_item:last-child .separator_icon {
+  display: none;
+}
+</style>
+<script>
+export default {
+  name: "Sobreadcrumb",
+  //   props: {
+  //     separator: {
+  //       type: "String",
+  //       default: "/"
+  //     }
+  //   },
+  props: ["separator", "separatorClass"],
+  /** separatorClass 为分割线的样式 */
+
+  data() {
+    return {};
+  },
+  provide() {
+    return {
+      Sobreadcrumb: this
+    };
+  },
+  methods: {}
+};
+</script>
+
+
